ea793ae137ed02272895dce69ac39ee7cec4adb8
Cecillya  Costa <cecillyagcosta@gmail.com>
2024-04-28 21:46:22 -0300

    0.0.2.3

--- Main/nc_to_pd/lab.py
+++ Main/nc_to_pd/lab.py
@@ -1,34 +1,12 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd 
+import numpy as np 
+#import tensorflow as tf
+import matplotlib.pyplot as plt 
+import netCDF4 as nc
 
-# Dados
-x1 = np.random.normal(size=100)
-y1 = np.random.normal(size=100)
-z1 = np.random.normal(size=100)
-
-x2 = np.random.normal(size=100)
-y2 = np.random.normal(size=100)
-z2 = np.random.normal(size=100)
-
-# Criar figura e eixos
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-
-# Scatter plot 1
-sc1 = ax1.scatter(x1, y1, c=z1, cmap='viridis')
-ax1.set_xlabel('X1')
-ax1.set_ylabel('Y1')
-ax1.set_title('Scatter Plot 1')
-fig.colorbar(sc1, ax=ax1, label='Z1')
-
-# Scatter plot 2
-sc2 = ax2.scatter(x2, y2, c=z2, cmap='viridis')
-ax2.set_xlabel('X2')
-ax2.set_ylabel('Y2')
-ax2.set_title('Scatter Plot 2')
-fig.colorbar(sc2, ax=ax2, label='Z2')
-
-# Ajustar espaçamento entre subplots
-plt.tight_layout()
-
-# Mostrar gráfico
+path = "C:/Users/cecil/Documents/GitHub/TensorMechanics/Sample/LWP.nc"
+data = nc.Dataset(path)
+LWP = data['LWP'][128,:,:] * 1000
+plt.imshow(LWP, vmin=1, vmax=400)
+plt.colorbar()
 plt.show()
